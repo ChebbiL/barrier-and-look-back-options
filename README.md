40bcde5440cbbde3886afa905d7398b9ffb2f0af
Thomas J. Espel <thomas.espel@orange.fr>
2018-02-24 09:37:46 +0000

    Updated: main.cpp Updated: main.cpp Updated: README.md

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ The team includes the following members, with the associated GitHub identifiers.
 | Name | GitHub id | Main allocation |
 | --- | --- | --- |
 |Tresnia Berah| tresnia | Delivery |
-|Thomas Espel|ddblue75| Code 2 |
+|Thomas Espel|tjespel| Code 2 |
 |Konstantin Kulak|suncomer| Code 1 |
 |Callum MacIver|maciverc| Code 2 |
 |Vera Zhang|xuanverazhang| Delivery |
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@ This si the main project file.
 
 #include<iostream> // For console output
 #include <cmath> // For math calculations
+#include <string> // For user instructions in greeks
 using namespace std;
-
 
 
 /*
@@ -36,26 +36,79 @@ public:
 Black-Scholes Call Class
 This class encapsultaes the BSCall.
 */
-class Black_Scholes{
+class call{
   // VARIABLES
-  double St,K,r,sigma,T,t;
+  double St, K, r, sigma, T, t;
+  double tau, discount;
+  int number_iterations;
   // UTILITY FUNCTIONS
-
+  // Computes the stock price using the direct method
+  double stock_price_single(){
+    return St * exp((r - 0.5*pow(sigma, 2)) * (T - t) + sigma * sqrt(T - t) * fun());
+  }
+  // Computes the stock price using the direct method with the random variable as an input
+  double stock_price_single(double Z){
+    return St * exp((r - 0.5*pow(sigma, 2)) * (T - t) + sigma * sqrt(T - t) * Z);
+  }
+  // Computes the call payoff using a single stock price (random)
+  double call_payoff_single(){
+    double current_value = stock_price_single();
+  	current_value > K ? current_value = discount*(current_value - K) : current_value = 0;
+  	return current_value;
+  }
   // MAIN FUNCTIONS
 
 public:
   // Constructor
-  Black_Scholes(double initial_stock_price = 100.0, double strike = 100.0, double interest_rate = 0.05, double volatility = 0.4,double time_final_T = 1, double time_initial_t = 0){
+  call(double initial_stock_price = 100.0, double strike = 100.0, double interest_rate = 0.05, double volatility = 0.4,double time_final_T = 1, double time_initial_t = 0){
 		St = initial_stock_price;
 		K = strike;
 		r = interest_rate;
 		sigma = volatility;
 		T = time_final_T;
 		t = time_initial_t;
+    tau = T - t;
+    discount = exp(-r*(T - t));
+    number_iterations = 1000;
   }
   // ACCESS FUNCTIONS
-
+  // Computes the call price by averaging (expectation) several (random) single call payoffs
+  double price(){
+    double result = .0;
+    for (int i=0; i++; i< number_iterations){
+      result += call_payoff_single();
+    }
+    return result/number_iterations;
+  }
+  /* Computes the delta according to the user input:
+  - 'pw' for pathwise derivatives estimates method
+  - 'lr' for likelihood ratios method
+  'lr' is regarded as most accurate, so built as default
+  */
+  double delta(string method){
+    if (method=='pw') {return delta_pw();}
+    return delta_lr();
+  }
   // SERVICE FUNCTIONS
+  // Computes delta using likelihood ratio method
+  double delta_lr(){
+    double result = .0, Z, current_value;
+  	for (long int i = 0; i < number_iterations; i++){
+  		Z = fun();
+  		current_value = stock_price_single(Z);
+  		(current_value > K) ? result += discount * (current_value - K) * Z : result += 0;
+  	}
+    return result / (St * sigma * sqrt(tau) * number_iterations);
+  }
+  // Computes delta using pathwise derivatives estimates method
+  double delta_pw(){
+    double result = 0.0, current_value;
+  	for (long int i = 0; i < number_iterations; i++){
+  		current_value = stock_price_single();
+  		(current_value > K / St) ? result += discount * current_value : result += 0;
+  	}
+    return result / number_iterations;
+  }
 };
 
 
