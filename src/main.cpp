866dc82520e74120ee595c47874c7cca08ca8a7b
Thomas J. Espel <thomas.espel@orange.fr>
2018-03-03 21:25:22 +0000

    Updated: src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -19,19 +19,23 @@ using namespace std;
 
 
 
-int debug_thomas(){
-    european_option call(100, 100, 0.05, 0.4, 1, 0, 10000);
-    cout<<"Theoretic price: "<<call.payoff_theoretic()<<endl;
-    cout<<"Theoretic delta: "<<call.delta("th")<<endl;
-    cout<<"Theoretic gamma: "<<call.gamma("th")<<endl;
+int debug_thomas(int x){
+  cout << "NUMBER OF SIMS: " << x << endl;
+  european_option call(100, 100, 0.05, 0.4, 1, 0, x);
+  cout<<"Theoretic price: "<<call.payoff_theoretic()<<endl;
+  cout<<"Theoretic delta: "<<call.delta("th")<<endl;
+  cout<<"Theoretic gamma: "<<call.gamma("th")<<endl;
+  cout<<" Theoretic vega: "<<call.vega("th")<<endl;
 
-    cout << "Simulation:"<<endl;
-    cout << "Price: " << call.price() << endl;
-    cout << "Delta: " << call.delta() << " (LR)" << endl;
-    cout << "Delta: " << call.delta("pw") << " (PW)" << endl;
-    cout << "Gamma: " << call.gamma() << " (PWLR)" << endl;
-    cout << "Gamma: " << call.gamma("lrpw") << " (LRPW)" << endl;
-    cout << "Gamma: " << call.gamma("lrlr") << " (LRLR)" << endl;
+  cout << "Simulation:"<<endl;
+  cout << "Price: " << call.price() << endl;
+  cout << "Delta: " << call.delta() << " (LR)" << endl;
+  cout << "Delta: " << call.delta("pw") << " (PW)" << endl;
+  cout << "Gamma: " << call.gamma() << " (PWLR)" << endl;
+  cout << "Gamma: " << call.gamma("lrpw") << " (LRPW)" << endl;
+  cout << "Gamma: " << call.gamma("lrlr") << " (LRLR)" << endl;
+  cout << "Vega : " <<  call.vega() << " (LR)" << endl;
+  cout << "Vega : " <<  call.vega("pw") << " (PW)" << endl;
 
     return 0;
 }
@@ -58,6 +62,8 @@ int debug_konstantin(){
 
 int main(){
     cout << "hello" << endl;
-    debug_konstantin();
+    debug_thomas(1000);
+    debug_thomas(10000);
+    debug_thomas(100000);
     return 0;
 }
