94d4c0387f153079975be61235feb87b411296f6
Thomas J. Espel <thomas.espel@orange.fr>
2018-03-09 11:08:22 +0000

    Updated: core task graphs

--- report/graphs/graphs_creator.py
+++ report/graphs/graphs_creator.py
@@ -9,12 +9,31 @@ data.tail()
 
 data["delta lr err"] = np.abs(data["delta lr"] - data["delta th"])
 
-fig, ax = plt.subplots()
-ax.semilogx([data[data.index==i]["delta lr err"].mean() for i in data.index.values], label='Error mean')
-ax.semilogx([data[data.index==i]["delta lr err"].var() for i in data.index.values], label='Error variance')
-ax.grid()
-plt.title('Analysis of delta pw error for the European call option')
-plt.xlabel('log(iterations)')
-plt.ylabel('value')
-plt.legend()
-plt.show()
+
+mytype = "euro"
+greeks = [('delta',['lr', 'pw']), ('gamma',['pwlr', 'lrpw', 'lrlr']), ('vega',['lr', 'pw'])]
+
+greeks_files_temp = [[greek[0]+'_'+greek[1][j] for j in range(len(greek[1]))] for greek in greeks]
+greeks_files = []
+for element in greeks_files_temp:
+    greeks_files += element
+
+greeks_names_temp = [[greek[0]+' '+greek[1][j] for j in range(len(greek[1]))] for greek in greeks]
+greeks_names = []
+for element in greeks_names_temp:
+    greeks_names += element
+
+
+for element in greeks_names:
+    data[str(element + " err")] = np.abs(data[element] - data[str(element.rsplit(' ', 1)[0] + " th")])
+    my_dpi = 72
+    plt.figure(figsize=(1000/my_dpi, 600/my_dpi), dpi=my_dpi)
+    fig, ax = plt.subplots()
+    ax.semilogx([data[data.index==i][element + " err"].mean() for i in data.index.values], label='Error mean')
+    ax.semilogx([data[data.index==i][element + " err"].var() for i in data.index.values], label='Error variance')
+    ax.grid()
+    plt.title('Analysis of ' + element + " error for the " + mytype + ' option')
+    plt.xlabel('log(iterations)')
+    plt.ylabel('value')
+    plt.legend()
+    plt.savefig(mytype + element + '.png', dpi=my_dpi)
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,10 @@ This is the main project file.
 #include <string> // For user instructions in greeks
 #include <random> // For random numbers generation
 
+
+#include <fstream>
+#include <ctime>
+
 using namespace std;
 
 
@@ -63,9 +67,90 @@ int debug_konstantin(){
     return 0;
 }
 
+
+int write_csv(string pathToFile){
+  ofstream myfile;
+  myfile.open(pathToFile);
+  myfile << "iterations;delta th;delta th time;delta lr;delta lr time;delta pw;delta pw time;gamma th;gamma th time;gamma pwlr;gamma pwlr time;gamma lrpw;gamma lrpw time;gamma lrlr;gamma lrlr time;vega th;vega th time;vega lr;vega lr time;vega pw;vega pw time\n";
+
+  european_option s1(100, 100, 0.05, 0.4, 1, 0, 1);
+
+  for (int i=1; i<10000000; i*=10){
+    for (int j=0; j<1000; j+=1){
+      s1 = european_option(100, 100, 0.05, 0.4, 1, 0, i);
+
+      clock_t begin = clock();
+      double delta_th = s1.delta("th");
+      clock_t end = clock();
+      double delta_th_time = double(end - begin) / CLOCKS_PER_SEC;
+      begin = clock();
+      double delta_lr = s1.delta("lr");
+      end = clock();
+      double delta_lr_time = double(end - begin) / CLOCKS_PER_SEC;
+      begin = clock();
+      double delta_pw = s1.delta("pw");
+      end = clock();
+      double delta_pw_time = double(end - begin) / CLOCKS_PER_SEC;
+
+
+      myfile << i << ";" << delta_th << ";" << delta_th_time;
+      myfile << ";" << delta_lr << ";" << delta_lr_time;
+      myfile << ";" << delta_pw << ";" << delta_pw_time;
+
+      begin = clock();
+      double gamma_th = s1.gamma("th");
+      end = clock();
+      double gamma_th_time = double(end - begin) / CLOCKS_PER_SEC;
+      begin = clock();
+      double gamma_pwlr = s1.gamma("pwlr");
+      end = clock();
+      double gamma_pwlr_time = double(end - begin) / CLOCKS_PER_SEC;
+      begin = clock();
+      double gamma_lrpw = s1.gamma("lrpw");
+      end = clock();
+      double gamma_lrpw_time = double(end - begin) / CLOCKS_PER_SEC;
+      begin = clock();
+      double gamma_lrlr = s1.gamma("lrlr");
+      end = clock();
+      double gamma_lrlr_time = double(end - begin) / CLOCKS_PER_SEC;
+
+
+      myfile << ";" << gamma_th << ";" << gamma_th_time;
+      myfile << ";" << gamma_pwlr << ";" << gamma_pwlr_time;
+      myfile << ";" << gamma_lrpw << ";" << gamma_lrpw_time;
+      myfile << ";" << gamma_lrlr << ";" << gamma_lrlr_time;
+
+      begin = clock();
+      double vega_th = s1.vega("th");
+      end = clock();
+      double vega_th_time = double(end - begin) / CLOCKS_PER_SEC;
+      begin = clock();
+      double vega_lr = s1.vega("lr");
+      end = clock();
+      double vega_lr_time = double(end - begin) / CLOCKS_PER_SEC;
+      begin = clock();
+      double vega_pw = s1.vega("pw");
+      end = clock();
+      double vega_pw_time = double(end - begin) / CLOCKS_PER_SEC;
+
+      myfile << ";" << vega_th << ";" << vega_th_time;
+      myfile << ";" << vega_lr << ";" << vega_lr_time;
+      myfile << ";" << vega_pw << ";" << vega_pw_time;
+
+
+      myfile << "\n";
+
+    }
+  }
+
+
+  return 0;
+}
+
+
 int main(){
     cout << "hello" << endl;
-    barrier_option boption(50, 100, 100, 0.05, 0.4, 1, 0, 1000);
+    /*barrier_option boption(50, 100, 100, 0.05, 0.4, 1, 0, 1000);
     cout << "Price: " << boption.price() << endl;
     cout << "Price: " << boption.price_theoretic() << " (Theoretical)" << endl;
     cout << "Delta: " << boption.delta() << " (LR)" << endl;
@@ -74,9 +159,9 @@ int main(){
     cout << "Gamma: " << boption.gamma("th") << " (Theoretical)" << endl;
     cout << "Vega: " << boption.vega() << " (LR)" << endl;
     cout << "Vega: " << boption.vega("th") << " (Theoretical)" << endl;
-
+*/
     //debug_thomas();
-    //write_csv(2);
-    debug_konstantin();
+    //write_csv("../report/graphs/core_task.csv");
+    //debug_konstantin();
     return 0;
 }
