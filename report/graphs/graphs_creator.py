7b91e57d90e668fa929b4157eacf57a280ed1275
Thomas J. Espel <thomas.espel@orange.fr>
2018-03-09 14:14:38 +0000

    Updated: graphs, solved issue #26

--- report/graphs/graphs_creator.py
+++ report/graphs/graphs_creator.py
@@ -3,11 +3,8 @@ import pandas as pd
 import numpy as np
 
 data = pd.DataFrame.from_csv('core_task.csv', sep=';')
-data.tail()
-
-
-
-data["delta lr err"] = np.abs(data["delta lr"] - data["delta th"])
+#data.tail()
+print('Data loaded.')
 
 
 mytype = "euro"
@@ -29,14 +26,33 @@ for element in greeks_names:
     my_dpi = 72
     plt.figure(figsize=(1000/my_dpi, 600/my_dpi), dpi=my_dpi)
     fig, ax = plt.subplots()
-    ax.semilogx([data[data.index==i][element + " err"].mean() for i in data.index.values], label='Error mean')
-    ax.semilogx([data[data.index==i][element + " err"].var() for i in data.index.values], label='Error variance')
+    ax.semilogx(data.index, [data[data.index==i][element + " err"].mean() for i in data.index.values], label='Error mean')
+    ax.semilogx(data.index,[data[data.index==i][element + " err"].var() for i in data.index.values], label='Error variance')
     ax.grid()
     plt.title('Analysis of ' + element + " error for the " + mytype + ' option')
-    plt.xlabel('log(iterations)')
+    plt.xlabel('Monte-Carlo Iterations')
     plt.ylabel('value')
     plt.legend()
     plt.savefig(mytype + element.replace(' ', '') + '.png', dpi=my_dpi)
+    print('Created: ' + mytype + element.replace(' ', '') + '.png')
+    plt.close()
+
+for element in greeks_names:
+    data[str(element + " err")] = np.abs(data[element] - data[str(element.rsplit(' ', 1)[0] + " th")])
+    my_dpi = 72
+    plt.figure(figsize=(1000/my_dpi, 600/my_dpi), dpi=my_dpi)
+    fig, ax = plt.subplots()
+    runtime = [data[data.index==i][element + " time"].mean() for i in data.index.values]
+    ax.semilogx(runtime, [data[data.index==i][element + " err"].mean() for i in data.index.values], label='Error mean')
+    ax.semilogx(runtime,[data[data.index==i][element + " err"].var() for i in data.index.values], label='Error variance')
+    ax.grid()
+    plt.title('Analysis of ' + element + " error for the " + mytype + ' option')
+    plt.xlabel('Computing time')
+    plt.ylabel('value')
+    plt.legend()
+    plt.savefig(mytype + element.replace(' ', '') + 'time.png', dpi=my_dpi)
+    print('Created: ' + mytype + element.replace(' ', '') + 'time.png')
+    plt.close()
 
 for element in greeks_names:
     print(element + ' error mean: ' + str(data[data.index==100000][element + " err"].mean()))
