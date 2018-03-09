d1a371d435ba91f551fcef494079cd905cfedd41
maciverc <36534423+maciverc@users.noreply.github.com>
2018-03-09 21:40:31 +0000

    Update lookback_option.cpp

--- src/lookback_option.cpp
+++ src/lookback_option.cpp
@@ -162,4 +162,99 @@ double d1(double moneyness, double r, double sigma, double T)
     return a/(sigma * sqrt(T));
 }
 
-//double lookback
+double lookback_option::Call_An()
+{
+    double d1 = d1(log(S_0/max(maxmin, K)), r, sigma);
+    double d2 = d1 - sigma*sqrt(tau);
+    double answer = St*normal_cdf(d1) - exp(-r*tau) *(normal_cdf(d2)*max(K,maxmin) + max(maxmin - K, 0.0)); 
+    double ans2 = -pow(St/(max(maxmin, K)), -2*r/(sigma*sigma))*normal_cdf(d1 - r/sigma*sqrt(tau));
+    ans2 += exp(r*tau)*normal_cdf(d1);
+    ans2 *= St *sigma*sigma*0.5/r;
+    ans2*= exp(-r*tau);
+    return ans + ans2;
+}
+
+////////////////////////
+
+#include <fstream>
+#include <ctime>
+
+int write_csv(string pathToFile){
+  ofstream myfile;
+  myfile.open(pathToFile);
+  myfile << "iterations;delta lr;delta lr time;delta pw;delta pw time;gamma pwlr;gamma pwlr time;vega lr;vega lr time;\n";
+
+  european_option s1(100, 100, 0.05, 0.4, 1, 0, 1);
+
+  for (int i=1; i<10000000; i*=10){
+    for (int j=0; j<1000; j+=1){
+      s1 = lookback_option(100, 100, 0.05, 0.4, 1, 0, i);
+
+   /*   clock_t begin = clock();
+      double delta_th = s1.delta("th");
+      clock_t end = clock();
+      double delta_th_time = double(end - begin) / CLOCKS_PER_SEC;
+     */ begin = clock();
+      double delta_lr = s1.delta_lr;
+      end = clock();
+      double delta_lr_time = double(end - begin) / CLOCKS_PER_SEC;
+      begin = clock();
+      double delta_pw = s1.delta_pw;
+      end = clock();
+      double delta_pw_time = double(end - begin) / CLOCKS_PER_SEC;
+
+
+      //myfile << i << ";" << delta_th << ";" << delta_th_time;
+      myfile << ";" << delta_lr << ";" << delta_lr_time;
+      myfile << ";" << delta_pw << ";" << delta_pw_time;
+
+     /* begin = clock();
+      double gamma_th = s1.gamma("th");
+      end = clock();
+      double gamma_th_time = double(end - begin) / CLOCKS_PER_SEC;
+    */begin = clock();
+      double gamma_pwlr = s1.gamma();
+      end = clock();
+      double gamma_pwlr_time = double(end - begin) / CLOCKS_PER_SEC;
+     /* begin = clock();
+      double gamma_lrpw = s1.gamma("lrpw");
+      end = clock();
+      double gamma_lrpw_time = double(end - begin) / CLOCKS_PER_SEC;
+      begin = clock();
+      double gamma_lrlr = s1.gamma("lrlr");
+      end = clock();
+      double gamma_lrlr_time = double(end - begin) / CLOCKS_PER_SEC;
+    */
+
+      //myfile << ";" << gamma_th << ";" << gamma_th_time;
+      myfile << ";" << gamma_pwlr << ";" << gamma_pwlr_time;
+      //myfile << ";" << gamma_lrpw << ";" << gamma_lrpw_time;
+      //myfile << ";" << gamma_lrlr << ";" << gamma_lrlr_time;
+
+      begin = clock();
+      double vega_th = s1.vega();
+      end = clock();
+      double vega_th_time = double(end - begin) / CLOCKS_PER_SEC;
+      /*begin = clock();
+      double vega_lr = s1.vega("lr");
+      end = clock();
+      double vega_lr_time = double(end - begin) / CLOCKS_PER_SEC;
+      begin = clock();
+      double vega_pw = s1.vega("pw");
+      end = clock();
+      double vega_pw_time = double(end - begin) / CLOCKS_PER_SEC;
+*/
+      myfile << ";" << vega_th << ";" << vega_th_time;
+     // myfile << ";" << vega_lr << ";" << vega_lr_time;
+      //myfile << ";" << vega_pw << ";" << vega_pw_time;
+
+
+      myfile << "\n";
+
+    }
+  }
+
+
+  return 0;
+}
+
